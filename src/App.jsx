0568c467288826f2100e17e2d6afd65a2523a9b9
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-17 14:54:09 -0500

    Refactor App component to use postCohereChat for API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import { postCohereChat } from "./services/cohereAPI";
+import { useState } from "react";
 
 function App() {
-  const COHERE_API_KEY = import.meta.env.COHERE_API_KEY;
-  const COHERE_API_URL = "https://api.cohere.com/v2/chat";
-  const [cohere, setCohere] = useState(null);
+  const [cohere, setCohere] = useState(null)
 
   useEffect(() => {
-    axios
-      .post(
-        COHERE_API_URL,
-        {
-          stream: false,
-          model: "command-a-03-2025",
-          messages: [{ role: "user", content: "Tell me about LLMs" }],
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${COHERE_API_KEY}`,
-            "Content-Type": "application/json",
-          },
-        }
-      )
-      .then((res) => {
-        if (!res.data) {
-          throw new Error("Error");
-        }
-        console.log(res.data);
-        setCohere(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [COHERE_API_KEY]);
+    postCohereChat(setCohere);
+  }, []);
 
   return (
     <main>
