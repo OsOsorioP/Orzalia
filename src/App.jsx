64be0efc13b85da0389bda033cb6aeac3ae19553
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-18 17:37:10 -0500

    Add RewriterTool component to App for text rewriting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import SummarizerTool from "./components/features/summarizer/SumarizerTool";
 import IdeaGeneratorTool from "./components/features/ideaGenerator/IdeaGeneratorTool";
+import RewriterTool from "./components/features/rewriter/RewriterTool"
 
 function App() {
   return (
     <main className="app-container">
       <SummarizerTool />
       <IdeaGeneratorTool />
+      <RewriterTool/>
+
     </main>
   );
 }
