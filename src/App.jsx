d59090edbd537e775804b075f43891aef54feba9
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-18 16:02:35 -0500

    Add IdeaGeneratorTool component to App for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import SummarizerTool from "./components/features/summarizer/SumarizerTool";
+import IdeaGeneratorTool from "./components/features/ideaGenerator/IdeaGeneratorTool";
 
 function App() {
   return (
     <main className="app-container">
       <SummarizerTool />
+      <IdeaGeneratorTool />
     </main>
   );
 }
