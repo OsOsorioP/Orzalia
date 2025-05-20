d533725b4bb18ac9ad9f5ef891c32be64947c6c0
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-19 21:08:31 -0500

    Refactor routing structure in App component and wrap App with BrowserRouter in main entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 import SummarizerTool from "./components/features/summarizer/SumarizerTool";
 import IdeaGeneratorTool from "./components/features/ideaGenerator/IdeaGeneratorTool";
-import RewriterTool from "./components/features/rewriter/RewriterTool"
+import RewriterTool from "./components/features/rewriter/RewriterTool";
 
 function App() {
   return (
     <main className="app-container">
-      <SummarizerTool />
-      <IdeaGeneratorTool />
-      <RewriterTool/>
-
+      <Routes>
+        <Route index element={<Navigate to={"/summarizer"} replace />} />
+        <Route path="summarizer" element={<SummarizerTool />} />
+        <Route path="idea_generator" element={<IdeaGeneratorTool />} />
+        <Route path="rewriter" element={<RewriterTool />} />
+      </Routes>
     </main>
   );
 }
