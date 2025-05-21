3bf8a62b841f74e042d278f9e051097584393dc3
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-21 13:40:50 -0500

    Refactor Button component to use CSS modules for styling

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,7 @@
-import "./Button.css";
+import styles from './Button.module.css'
 
 const Button = (name) => {
-  return <button>{name}</button>;
+  return <button className={styles.myButton}>{name}</button>;
 };
 
 export default Button;
