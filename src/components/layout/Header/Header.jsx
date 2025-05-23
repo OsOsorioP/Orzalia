04f878c8213787ccc9af3e64948281988ebca2c4
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-23 14:46:26 -0500

    Add Footer component and restore Header component with navigation links

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../Navbar/Navbar";
+import logo from "../../../assets/react.svg";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className={styles.appHeader}>
+      <Link to="/">
+        <picture className={styles.logoContainer}>
+          <img className={styles.logo} src={logo} alt="" />
+          <h1 className={""}>Rexi Toolkit</h1>
+        </picture>
+      </Link>
+
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
