451a67191fce25e4501f561c3b0e0da9485a665c
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-23 15:57:58 -0500

    Refactor Navbar component to use NavLink for navigation and improve code structure

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
-import styles from "./Navbar.module.css"
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const navLinks = [
+    { to: "/summarizer", name: "Summarizer" },
+    { to: "/idea_generator", name: "Idea Generator" },
+    { to: "/rewriter", name: "Rewriter" },
+  ];
+
+  const getNavLinkClass = ({ isActive }) => {
+    return isActive ? `${styles.navLink} ${styles.active}` : styles.navLink;
+  };
+
   return (
     <nav className={styles.appNavbar}>
-      <Link to={"summarizer"}>Summarizer</Link>
-      <Link to={"idea_generator"}>Idea Generator</Link>
-      <Link to={"rewriter"}>Rewriter</Link>
+      <ul className={styles.navList}>
+        {navLinks.map((link, index) => (
+          <li key={index} className={styles.navItem}>
+            <NavLink to={link.to} className={getNavLinkClass}>
+              {link.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
