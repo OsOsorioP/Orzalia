81afd539b1b70ff4a59c37f9b5ac995c0f4d1949
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-23 14:46:38 -0500

    Refactor App component structure to include Header and Footer, and update routing to use HomePage as the default

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import "./App.css";
-import { Route, Routes, Navigate } from "react-router-dom";
-import SummarizerTool from "./components/features/summarizer/SumarizerTool";
-import IdeaGeneratorTool from "./components/features/ideaGenerator/IdeaGeneratorTool";
-import RewriterTool from "./components/features/rewriter/RewriterTool";
+import { Route, Routes } from "react-router-dom";
+import SummarizerTool from "./features/summarizer/SumarizerTool";
+import IdeaGeneratorTool from "./features/ideaGenerator/IdeaGeneratorTool";
+import RewriterTool from "./features/rewriter/RewriterTool";
+import HomePage from "./pages/HomePage";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
 
 function App() {
   return (
-    <main className="app-container">
-      <Routes>
-        <Route index element={<Navigate to={"/summarizer"} replace />} />
-        <Route path="summarizer" element={<SummarizerTool />} />
-        <Route path="idea_generator" element={<IdeaGeneratorTool />} />
-        <Route path="rewriter" element={<RewriterTool />} />
-      </Routes>
-    </main>
+    <>
+      <Header />
+      <main className="app-container">
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="summarizer" element={<SummarizerTool />} />
+          <Route path="idea_generator" element={<IdeaGeneratorTool />} />
+          <Route path="rewriter" element={<RewriterTool />} />
+        </Routes>
+        <Footer />
+      </main>
+    </>
   );
 }
 
