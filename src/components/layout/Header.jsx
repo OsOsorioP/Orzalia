44611b53cc2cfd3a90e38651a3aba956c09ae607
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-22 21:17:08 -0500

    Update Header component to include navigation links and change header title

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header>
-    Menu de naveación
-  </header>;
+  return (
+    <header>
+      <h1>Herramientas</h1>
+      <Link to={"summarizer"}>Summarizer</Link>
+      <Link to={"idea_generator"}>Idea Generator</Link>
+      <Link to={"rewriter"}>Rewriter</Link>
+    </header>
+  );
 };
 
 export default Header;
