657be7559b00c5c023573622e02fe7a6eff9e94c
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-24 10:20:41 -0500

    Refactor Footer component to enhance layout and improve content structure

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,38 @@
+import logo from "../../../assets/react.svg";
+import styles from "./Footer.module.css";
+
 const Footer = () => {
-  return <footer>Pie de página</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.descriptionPlataform}>
+        <div className={styles.sectionTools}>
+          <ul>
+            <li>Resumidor</li>
+            <li>Reescritor</li>
+            <li>Generador de Ideas</li>
+          </ul>
+        </div>
+        <div className={styles.sectionLogo}>
+          <picture className={styles.logo}>
+            <img src={logo} alt="Logo de Rexi Toolkit" />
+            <h1>Rexi Toolkit</h1>
+          </picture>
+          <p>
+            Rexi Toolkit es una paltaforma web que orefece herramientas para
+            crear y gestionar contenido textual, dirigida al publico en general.
+            Incluye servicios para resumir, reescritura y generador de ideas.
+          </p>
+        </div>
+      </div>
+      <div className={styles.footerDown}>
+        <p>Rexi Toolkit 2025</p>
+        <div className={styles.socialNet}>
+          <i>GitHub</i>
+          <i>Linkedlin</i>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
