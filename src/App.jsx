067313476484a5fb0056e457fe5b7f3a848433a0
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-25 19:13:49 -0500

    Refactor App component to streamline routing and remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import SummarizerTool from "./features/summarizer/SumarizerTool";
 import IdeaGeneratorTool from "./features/ideaGenerator/IdeaGeneratorTool";
 import RewriterTool from "./features/rewriter/RewriterTool";
-import HomePage from "./pages/HomePage";
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
+import SummarizerPage from "./pages/SummarizerPage";
+import IdeageneratorPage from "./pages/IdeaGeneratorPage";
+import RewritePage from "./pages/RewriterPage";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="app-container">
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="summarizer" element={<SummarizerTool />} />
-          <Route path="idea_generator" element={<IdeaGeneratorTool />} />
-          <Route path="rewriter" element={<RewriterTool />} />
-        </Routes>
-        <Footer />
-      </main>
+      <Routes>
+        <Route index element={<SummarizerPage />} />
+        <Route path="idea_generator" element={<IdeageneratorPage />} />
+        <Route path="rewriter" element={<RewritePage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
