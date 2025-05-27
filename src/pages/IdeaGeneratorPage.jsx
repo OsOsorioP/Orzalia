f23b757b145880a9bef4ac4e1acae94dfc0b4383
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-27 15:29:53 -0500

    Refactor IdeaGeneratorPage to replace Info components with a Card component for improved feature display

--- src/pages/IdeaGeneratorPage.jsx
+++ src/pages/IdeaGeneratorPage.jsx
@@ -1,31 +1,55 @@
+import Card from "../components/ui/Card/Card";
 import IdeaGeneratorTool from "../features/ideaGenerator/IdeaGeneratorTool";
-import Info from "../components/common/Info/Info";
 import { ClockIcon } from "@heroicons/react/24/outline";
+import { BoltIcon } from "@heroicons/react/24/outline";
+import { FireIcon } from "@heroicons/react/24/outline";
+import { UsersIcon } from "@heroicons/react/24/outline";
 
 const IdeageneratorPage = () => {
+  const featuresIdeaGenerator = [
+    {
+      icon: <BoltIcon />,
+      title: "Supera el Bloqueo Creativo",
+      description:
+        "¿Atascado sin ideas? Introduce un tema y deja que nuestra IA te proporcione múltiples ángulos y enfoques para tu próximo contenido.",
+      iconBgColor: "bgBlue100",
+      iconTextColor: "textBlue600",
+    },
+    {
+      icon: <FireIcon />,
+      title: "Variedad de Sugerencias",
+      description:
+        "Obtén desde títulos llamativos hasta conceptos más desarrollados, adaptados al tipo de contenido que necesitas.",
+      iconBgColor: "bgOrange100",
+      iconTextColor: "textOrange600",
+    },
+    {
+      icon: <UsersIcon />,
+      title: "Personaliza tu Búsqueda",
+      description:
+        "Especifica la cantidad de ideas y el tipo de contenido (blogs, videos, redes sociales) para resultados más afinados.",
+      iconBgColor: "bgPurple100",
+      iconTextColor: "textPurple600",
+    },
+    {
+      icon: <ClockIcon />,
+      title: "Enfócate en tu Audiencia",
+      description:
+        "Genera ideas que resuenen con tu público objetivo y aborden sus intereses y necesidades.",
+      iconBgColor: "bgGreen100",
+      iconTextColor: "textGreen600",
+    },
+  ];
+
   return (
     <main className="app-container">
       <IdeaGeneratorTool />
-      <Info
-      title={"¿Cómo te ayuda el Generador de Ideas?"}
-        subtitle="Supera el Bloqueo Creativo"
-        description="¿Atascado sin ideas? Introduce un tema y deja que nuestra IA te proporcione múltiples ángulos y enfoques para tu próximo contenido."
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Variedad de Sugerencias"
-        description="Obtén desde títulos llamativos hasta conceptos más desarrollados, adaptados al tipo de contenido que necesitas."
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Personaliza tu Búsqueda"
-        description="Especifica la cantidad de ideas y el tipo de contenido (blogs, videos, redes sociales) para resultados más afinados."
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Enfócate en tu Audiencia"
-        description="Genera ideas que resuenen con tu público objetivo y aborden sus intereses y necesidades."
-        icon={<ClockIcon />}
+      <Card
+        title={"¿Cómo te ayuda el Generador de Ideas?"}
+        description={
+          " Descubre todas las ventajas de nuestra herramienta potenciada por IA"
+        }
+        features={featuresIdeaGenerator}
       />
     </main>
   );
