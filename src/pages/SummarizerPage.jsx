613398b437dfb484c6c327dae604e579e838497b
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-27 15:29:57 -0500

    Refactor SummarizerPage to replace Info components with a Card component for enhanced feature presentation

--- src/pages/SummarizerPage.jsx
+++ src/pages/SummarizerPage.jsx
@@ -1,33 +1,54 @@
-import Info from "../components/common/Info/Info";
+import Card from "../components/ui/Card/Card";
 import SummarizerTool from "../features/summarizer/SumarizerTool";
 import { ClockIcon } from "@heroicons/react/24/outline";
+import { BoltIcon } from "@heroicons/react/24/outline";
+import { FireIcon } from "@heroicons/react/24/outline";
+import { UsersIcon } from "@heroicons/react/24/outline";
 
 const SummarizerPage = () => {
+  const featuresSummarizer = [
+    {
+      icon: <BoltIcon />,
+      title: "Supera el Bloqueo Creativo",
+      description:
+        "¿Atascado sin ideas? Introduce un tema y deja que nuestra IA te proporcione múltiples ángulos y enfoques para tu próximo contenido.",
+      iconBgColor: "bgBlue100",
+      iconTextColor: "textBlue600",
+    },
+    {
+      icon: <FireIcon />,
+      title: "Variedad de Sugerencias",
+      description:
+        "Obtén desde títulos llamativos hasta conceptos más desarrollados, adaptados al tipo de contenido que necesitas.",
+      iconBgColor: "bgOrange100",
+      iconTextColor: "textOrange600",
+    },
+    {
+      icon: <UsersIcon />,
+      title: "Personaliza tu Búsqueda",
+      description:
+        "Especifica la cantidad de ideas y el tipo de contenido (blogs, videos, redes sociales) para resultados más afinados.",
+      iconBgColor: "bgPurple100",
+      iconTextColor: "textPurple600",
+    },
+    {
+      icon: <ClockIcon />,
+      title: "Enfócate en tu Audiencia",
+      description:
+        "Genera ideas que resuenen con tu público objetivo y aborden sus intereses y necesidades.",
+      iconBgColor: "bgGreen100",
+      iconTextColor: "textGreen600",
+    },
+  ];
   return (
     <main className="app-container">
       <SummarizerTool />
-      <Info
-        title={"¿Por qué usar nuestro Resumidor?"}
-        subtitle={"Ahorra Tiempo Precioso"}
+      <Card
+        title={"¿Cómo te ayuda el Generador de Ideas?"}
         description={
-          "Obtén la esencia de textos largos en segundos, ideal para investigadores, estudiantes y profesionales ocupados."
+          " Descubre todas las ventajas de nuestra herramienta potenciada por IA"
         }
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Resúmenes Flexibles"
-        description="Elige entre resúmenes cortos, medios o largos para adaptar la información a tus necesidades específicas."
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Claridad y Precisión"
-        description="Nuestra IA está entrenada para extraer los puntos clave y presentarlos de manera coherente y fácil de entender."
-        icon={<ClockIcon />}
-      />
-      <Info
-        subtitle="Soporte Multilingüe"
-        description="Capaz de procesar y resumir textos en diversos idiomas."
-        icon={<ClockIcon />}
+        features={featuresSummarizer}
       />
     </main>
   );
