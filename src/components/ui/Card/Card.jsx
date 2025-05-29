63aa1140c0382a9219d45285cbb3b4c643f5b209
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-28 20:19:08 -0500

    Refactor Card component to simplify structure and enhance reusability

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -1,35 +1,7 @@
 import styles from "./Card.module.css";
 
-const Card = ({ title, description, features = [] }) => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.cardHeader}>
-        <h2 className={styles.cardTitle}> {title} </h2>
-
-        <p className={styles.cardDescription}>{description}</p>
-      </div>
-      <div className={styles.cardContent}>
-        <div className={styles.featuresGrid}>
-          {features &&
-            features.map((feature) => (
-              <div className={styles.featureItem}>
-                <div
-                  className={`${styles.iconContainer} ${
-                    styles[feature.iconBgColor]
-                  } ${styles[feature.iconTextColor]}`}
-                >
-                  {feature.icon}
-                </div>
-                <h3 className={styles.featureTitle}>{feature.title}</h3>
-                <p className={styles.featureDescription}>
-                  {feature.description}
-                </p>
-              </div>
-            ))}
-        </div>
-      </div>
-    </div>
-  );
+const Card = ({ children, className = "" }) => {
+  return <div className={`${styles.card} ${className}`}>{children}</div>;
 };
 
 export default Card;
