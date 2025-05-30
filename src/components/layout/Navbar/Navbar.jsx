e7c5aa768e14cde667884f2e38d6cf67e2f4a563
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-29 19:43:03 -0500

    Add icons to navigation links in Navbar component

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { XMarkIcon, Bars3Icon } from "@heroicons/react/24/outline";
+import {
+  XMarkIcon,
+  Bars3Icon,
+  PencilIcon,
+  PaintBrushIcon,
+  LightBulbIcon,
+} from "@heroicons/react/24/outline";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navLinks = [
-    { to: "/", name: "Resumidor" },
-    { to: "/idea_generator", name: "Generador de ideas" },
-    { to: "/rewriter", name: "Reescritor" },
+    { to: "/", name: "Resumidor", icon: <PencilIcon width={"1.5rem"} /> },
+    {
+      to: "/idea_generator",
+      name: "Generador de ideas",
+      icon: <LightBulbIcon width={"1.5rem"} />,
+    },
+    {
+      to: "/rewriter",
+      name: "Reescritor",
+      icon: <PaintBrushIcon width={"1.5rem"} />,
+    },
   ];
 
   const getNavLinkClass = ({ isActive }) => {
@@ -27,17 +41,14 @@ const Navbar = () => {
           {navLinks.map((link, index) => (
             <li key={index} className={styles.navItem}>
               <NavLink to={link.to} className={getNavLinkClass}>
-                {link.name}
+                {link.icon}
+                <span>{link.name}</span>
               </NavLink>
             </li>
           ))}
         </ul>
       </nav>
-      <button
-        className={styles.menu}
-        onClick={toggleMenu}
-        aria-label="Menu"
-      >
+      <button className={styles.menu} onClick={toggleMenu} aria-label="Menu">
         {isMenuOpen ? (
           <div className={styles.iconMenu}>
             <XMarkIcon className={styles.icon} />
@@ -51,7 +62,12 @@ const Navbar = () => {
       {isMenuOpen && (
         <nav className={styles.menuNav}>
           {navLinks.map((link, index) => (
-            <NavLink key={index} to={link.to} className={getNavLinkClass} onClick={toggleMenu}>
+            <NavLink
+              key={index}
+              to={link.to}
+              className={getNavLinkClass}
+              onClick={toggleMenu}
+            >
               {link.name}
             </NavLink>
           ))}
