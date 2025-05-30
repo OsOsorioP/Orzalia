e68696472adb2abcf23a5834223a17e81cedac7d
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-30 13:51:42 -0500

    Replace fallback message in Suspense with Loader component for improved loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
 import RoutesPages from "./utils/routers/RoutesPages";
 import "./App.css";
+import Loader from "./components/common/Loader/Loader";
 
 function App() {
   return (
     <>
-      <Suspense fallback={<div>Cargando pagina...</div>}>
+      <Suspense fallback={<Loader />}>
         <Header />
         <RoutesPages />
         <Footer />
