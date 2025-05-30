c3253383d5fffa0d668247b273c8cda836c6fda9
workspace014-admin <oscar.osorio1884@gmail.com>
2025-05-30 13:51:57 -0500

    Refactor Footer component: unify scrollToTop function usage in NavLinks

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -3,17 +3,25 @@ import styles from "./Footer.module.css";
 import Logo from "../../common/Logo/Logo";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.descriptionPlataform}>
         <div className={styles.sectionTools}>
           <h2>Herramientas</h2>
           <nav>
-            <NavLink to="/" onClick={window}>
+            <NavLink to="/" onClick={scrollToTop}>
               Resumidor
             </NavLink>
-            <NavLink to="/rewriter">Reescritor</NavLink>
-            <NavLink to="/idea_generator">Generador de Ideas</NavLink>
+            <NavLink to="/rewriter" onClick={scrollToTop}>
+              Reescritor
+            </NavLink>
+            <NavLink to="/idea_generator" onClick={scrollToTop}>
+              Generador de Ideas
+            </NavLink>
           </nav>
         </div>
         <div className={styles.sectionTools}>
@@ -25,7 +33,11 @@ const Footer = () => {
         <div className={styles.sectionLogo}>
           <Logo className={styles.logo} />
           <p>
-            Orzalia es una plataforma web diseñada con herramientas que facilitan la creación y gestión de contenido textual, dirigida al público en general. Entre sus servicios se incluyen funciones para resumir, reescribir y generar ideas, lo que la convierte en una herramienta versátil para diversos usuarios.  
+            Orzalia es una plataforma web diseñada con herramientas que
+            facilitan la creación y gestión de contenido textual, dirigida al
+            público en general. Entre sus servicios se incluyen funciones para
+            resumir, reescribir y generar ideas, lo que la convierte en una
+            herramienta versátil para diversos usuarios.
           </p>
         </div>
       </div>
