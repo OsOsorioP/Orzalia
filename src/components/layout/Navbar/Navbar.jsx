87882868b99cff1b5ac57711aa85069afdbbede8
workspace014-admin <oscar.osorio1884@gmail.com>
2025-06-04 08:00:45 -0500

    Smooth scroll to top on navigation link click

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -27,7 +27,10 @@ const Navbar = () => {
   ];
 
   const scrollToTop = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
 
   const getNavLinkClass = ({ isActive }) => {
